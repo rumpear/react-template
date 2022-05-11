213e7ff6c0820346022a31ab8d68d2cde2ed399a
Roman <77742141+rumpear@users.noreply.github.com>
2022-05-11 16:20:45 +0300

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+// import { Component } from 'react';
+
+import { Section } from './Section';
+import { Container } from './Container';
+
+// * all
+const App = () => (
+  <Section>
+    <h1>Hello</h1>
+  </Section>
+);
+
+export default App;
